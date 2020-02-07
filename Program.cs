18d06d9e3148de1f8cb9f395f2f10831bd93b3c7
prasadacharya <prasada@mail.usf.edu>
2020-02-07 12:02:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InClassExerciseOOP
+{
+    // used multiple classes
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // created multiple objects - audi , bmw
+
+            Car audi = new Car();
+            Car bmw = new Car();
+            Console.WriteLine(audi.color);
+            Console.WriteLine(audi.maxSpeed);
+            Console.WriteLine(bmw.color);
+            Console.WriteLine(bmw.maxSpeed);
+        }
+    }
+
+    //Created a Class Car
+    class Car
+    {
+        //class members
+        //created a variable(property) color , maxspeed
+        public string color = "red"; // field
+        public int maxSpeed = 200;   // field
+
+        public void fullThrottle()   // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
